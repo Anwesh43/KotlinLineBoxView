c16dcf1e0d0879c4f67a989a43b617cde162fd1a
Anwesh43 <anweshthecool0@gmail.com>
2018-03-26 02:09:06 +0530

    adding LineBoxView class

--- lineboxview/src/main/java/ui/anwesome/com/lineboxview/LineBoxView.kt
+++ lineboxview/src/main/java/ui/anwesome/com/lineboxview/LineBoxView.kt
@@ -1,5 +1,25 @@
 package ui.anwesome.com.lineboxview
 
+import android.content.Context
+import android.view.MotionEvent
+import android.view.View
+import android.graphics.Paint
+import android.graphics.Canvas
+
 /**
  * Created by anweshmishra on 26/03/18.
  */
+class LineBoxView(ctx : Context) : View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
