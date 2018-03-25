9f98da81caca353c77cc19970a11ac48b21aa91e
Anwesh43 <anweshthecool0@gmail.com>
2018-03-26 02:29:06 +0530

    adding State class

--- lineboxview/src/main/java/ui/anwesome/com/lineboxview/LineBoxView.kt
+++ lineboxview/src/main/java/ui/anwesome/com/lineboxview/LineBoxView.kt
@@ -22,4 +22,25 @@ class LineBoxView(ctx : Context) : View(ctx) {
         }
         return true
     }
+    data class State (var prevScale : Float = 0f, var dir : Int = 0, var j : Int = 0) {
+        val scales : Array<Float> = arrayOf(0f, 0f)
+        fun update(stopcb : (Float) -> Unit) {
+            scales[j] += dir * 0.1f
+            if (Math.abs(scales[j] - prevScale) > 1) {
+                scales[j] = prevScale + dir
+                j += dir
+                if (j == scales.size || j == -1) {
+                    j -= dir
+                    dir = 0
+                    stopcb(prevScale)
+                }
+            }
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            if (dir == 0) {
+                dir = 1 - 2 * prevScale.toInt()
+                startcb()
+            }
+        }
+    }
 }
